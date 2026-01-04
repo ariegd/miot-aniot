feb8fe71faf1f296b9a46ba967d1b2e621b2724e
zodd <zodd@nootbster.home>
2026-01-04 19:38:06 +0100

    Agregar imu componentes con errores

--- src/labF/hello_world/main/hello_world_main.c
+++ src/labF/hello_world/main/hello_world_main.c
@@ -15,6 +15,7 @@
 #include "esp_log.h"
 #include "blink_comp.h"
 #include "sht3c_comp.h"
+#include "imu_comp.h"
 
 static const char *TAG = "hello_world";
 
@@ -23,6 +24,7 @@ void app_main(void)
     ESP_LOGI(TAG,  "Hello world!");
     blink_start();
     shtc3_start();
+    imu_start();
   
     /* Print chip information */
 /*    esp_chip_info_t chip_info;
